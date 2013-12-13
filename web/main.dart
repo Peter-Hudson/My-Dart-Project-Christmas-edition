0b0c6de4df0f648174b090b6eb3614989a5052b1
Peter <peterhhudson@gmail.com>
2013-12-13 16:00:34 +0000

    slight rename

--- web/main.dart
+++ web/main.dart
@@ -9,10 +9,29 @@ import 'mymvc.dart';
 import 'dart:html';
 import 'dart:js' show context, JsObject;
 
+var test;
+
 void main() {
   
-  final Snap = context['Snap'];
-  var test = new JsObject(Snap, ['#svg']);
-  test.callMethod('circle',[200,200,100]);
   
+  final Snap = context['Snap'];
+  test = new JsObject(Snap, ['#svg']);
+//  test.callMethod('circle',[200,200,100]);
+  Snap.callMethod('load',['christmas-tree.svg', (e){
+    var g = e.callMethod('select',['#svg3025']);
+    Snap.callMethod('load',['star.svg',(f){
+      var scoped = new JsObject.jsify({
+        'x1':100,
+        'y1':100
+      }); 
+      var sg = f.callMethod('select',['#svg2']);
+      g.callMethod('append',[sg]);
+    }]);
+    test.callMethod('append',[g]);
+  }]);
+
+  
+  
+    
 }
+
