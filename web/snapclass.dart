c808dca2e9186d927103ea6cce8b7d4f5d19055d
Peter <peterhhudson@gmail.com>
2013-12-19 12:59:30 +0000

    Have a look at home

--- web/snapclass.dart
+++ web/snapclass.dart
@@ -24,29 +24,70 @@ snapclass(List args) {
     DefineImages(args,e);
     paper.callMethod('prepend',[tree]);
   }]);
+  
 }
 
  void DefineImages(List pasd_list_images,pe){   
-   var imageattr = new JsObject.jsify({
+   var patternattr = new JsObject.jsify({
      'x':0,
-     'y':0,
-     'width':175,
-     'height':140  
+     'y':0,     
+     'width':150,
+     'height':150,
+//     'viewbox': '0 0 150 150',
+     'patternUsertUnits': 'objectBoundingBox',
+     'preserveAspectRatio': 'xMidYMid slice',     
    });
+   
+   
    
    
    for(var i = 0; i< image_points.length; i++){
      if(i < pasd_list_images.length){    
-       var temp_ob = paper.callMethod('image',[pasd_list_images[i],0,0,120,120]);       
-       var def_temp_ob = temp_ob.callMethod('pattern',[imageattr]).callMethod('toDefs');
+       var selected_image = image_points[i];
+//       var def_temp_ob = temp_ob.callMethod('pattern',[patternattr]).callMethod('toDefs');
        var imagebg = new JsObject.jsify({
-         'fill': def_temp_ob
+//         'ClipPath': def_temp_ob,
+//         'viewbox': '75 75 150 150',
+//         'preserveAspectRatio': 'xMaxYMax slice',
+//         'transform':'scale(1)',
+         'fill':'#000'
        });
 //       pe.callMethod('attr',[imagebg]);
-       var selected = pe.callMethod('select',[image_points[i]]);
-       selected.callMethod('attr',[imagebg]);
-       print(def_temp_ob);
-       image_list.add(def_temp_ob);
+       var selected = pe.callMethod('select',[selected_image]);
+//       var ppp = selected.callMethod('attr',[imagebg]);
+//       var details = ppp.callMethod('attr',['fill']);
+//       print(ppp);
+       var test = selected.callMethod('getBBox');
+       
+       var temp_ob = paper.callMethod('image',[pasd_list_images[i]]);
+       var imageattr = new JsObject.jsify({ 
+//         'x':test['x'],
+//         'y':0,
+//         'width': 264,
+//         'height': 253,
+         
+         'clip-path': 'url(${selected_image}c)'
+           
+       });
+       
+       var newbox = test;
+       newbox['clip-path'] = 'url(#starsvgc)';
+       temp_ob.callMethod('attr',[imageattr]); 
+       temp_ob.callMethod('drag');       
+       print(test.runtimeType);
+       print(test['width']);
+       print(test['vb']);
+       print(test['height']);
+       print(test['h']);
+       print(test['x']);
+       print(test['y']);
+       print((test['path']));
+       
+       
+       
+        
+       
+       image_list.add(temp_ob);
      }
    }
            
